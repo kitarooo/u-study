5a4a84073e2987141a4f4f6b2c45a074b3266edc
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-08-06 12:59:44 +0500

    first commit

---
+++ src/main/java/com/backend/ustudy/UStudyApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ustudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/ustudy/UStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ustudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
